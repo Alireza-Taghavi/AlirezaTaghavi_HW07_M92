8927e92c28fac671e2588e8e685e04bf9a3ff31d
Alireza Taghavi <taghavizadeh.alireza@gmail.com>
2023-03-16 03:06:16 +0330

    fix(Q2): checks if constructor has at least 2 metals and 2 non-metals

--- Q2/src/main/java/org/example/ElementArray.java
+++ Q2/src/main/java/org/example/ElementArray.java
@@ -5,6 +5,21 @@ public class ElementArray {
 
     public ElementArray(Element[] elements) {
         this.elements = elements;
+
+        int metalCount = 0;
+        int nonMetalCount = 0;
+
+        for (Element element : elements) {
+            if (element instanceof MetalElement) {
+                metalCount++;
+            } else if (element instanceof NonMetalElement) {
+                nonMetalCount++;
+            }
+        }
+
+        if (metalCount < 2 || nonMetalCount < 2) {
+            throw new IllegalArgumentException("There must be at least two MetalElement objects and two NonMetalElement objects in the array");
+        }
     }
 
     public void displayElements() {
@@ -14,4 +29,3 @@ public class ElementArray {
         }
     }
 }
-
