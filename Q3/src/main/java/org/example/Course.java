882f0f6f8d6853ed4a3558228fca1a387df9fb56
Alireza Taghavi <taghavizadeh.alireza@gmail.com>
2023-03-16 03:29:12 +0330

    feat(Q3): Course class created

---
+++ Q3/src/main/java/org/example/Course.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Course {
+    private String name;
+    private Professor professor;
+    private List<Student> students;
+    private List<Grade> grades;
+
+    public Course(String name) {
+        this.name = name;
+        this.students = new ArrayList<>();
+        this.grades = new ArrayList<>();
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public void setProfessor(Professor professor) {
+        this.professor = professor;
+        professor.teach(this);
+    }
+
+    public void grade(Student student, int value) {
+        grades.add(new Grade(student, value));
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public List<Grade> getGrades() {
+        return grades;
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+}
